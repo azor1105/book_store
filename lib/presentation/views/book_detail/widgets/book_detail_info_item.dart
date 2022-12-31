4449877abda5d158d4c9ee881a83a613b4c328d2
azor1105 <azambekergashev1@gmail.com>
2022-12-31 10:32:35 +0500

    some changes

--- lib/presentation/views/book_detail/widgets/book_detail_info_item.dart
+++ lib/presentation/views/book_detail/widgets/book_detail_info_item.dart
@@ -1,7 +1,7 @@
 import 'package:book_store/data/models/book/book_model.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
 import '../../../utils/my_fonts.dart';
 
 class BookDetailInfoItem extends StatelessWidget {
