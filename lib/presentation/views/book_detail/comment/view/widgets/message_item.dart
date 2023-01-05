8cf6f8d7635230cee170e31627a805afc6eb40fb
azor1105 <azambekergashev1@gmail.com>
2023-01-05 15:32:22 +0500

    fixed bug on message item

--- lib/presentation/views/book_detail/comment/view/widgets/message_item.dart
+++ lib/presentation/views/book_detail/comment/view/widgets/message_item.dart
@@ -28,81 +28,82 @@ class MessageItem extends StatelessWidget {
         Padding(
           padding: EdgeInsets.only(bottom: 8.h),
           child: SizedBox(
-            width: MediaQuery.of(context).size.width * 0.7,
+            width: MediaQuery.of(context).size.width*0.7,
             child: Row(
-              mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.end,
               mainAxisAlignment:
                   isUser ? MainAxisAlignment.end : MainAxisAlignment.start,
               children: [
                 !isUser ? photoItem() : const SizedBox(),
-                Stack(
-                  children: [
-                    Align(
-                      alignment:
-                          isUser ? Alignment.topRight : Alignment.topLeft,
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                          right: isUser ? 7.w : 0,
-                          left: !isUser ? 7.w : 0,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: isUser
-                              ? CrossAxisAlignment.end
-                              : CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              DateFormat.Hm().format(
-                                userComment.commentModel.createdAt,
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: isUser ? Alignment.topRight : Alignment.topLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            right: isUser ? 7.w : 0,
+                            left: !isUser ? 7.w : 0,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: isUser
+                                ? CrossAxisAlignment.end
+                                : CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                DateFormat.Hm().format(
+                                  userComment.commentModel.createdAt,
+                                ),
+                                style: PoppinsFont.w400.copyWith(
+                                  fontSize: 14.sp,
+                                  color: ColorConst.blackWithOpacity063,
+                                ),
                               ),
-                              style: PoppinsFont.w400.copyWith(
-                                fontSize: 14.sp,
-                                color: ColorConst.blackWithOpacity063,
-                              ),
-                            ),
-                            SizedBox(height: 6.h),
-                            Container(
-                              padding: const EdgeInsets.all(12),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(12.r),
-                                color: !isUser
-                                    ? ColorConst.cE5E5E5
-                                    : ColorConst.c8875FF,
-                              ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  if (!isUser && showUserName)
-                                    Padding(
-                                      padding: EdgeInsets.only(bottom: 5.h),
-                                      child: Text(
-                                        userComment.userModel.fullName,
-                                        style: PoppinsFont.w600.copyWith(
-                                          fontSize: 15.sp,
-                                          color: isUser
-                                              ? ColorConst.white
-                                              : ColorConst.blackWithOpacity087,
+                              SizedBox(height: 6.h),
+                              Container(
+                                padding: const EdgeInsets.all(12),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12.r),
+                                  color: !isUser
+                                      ? ColorConst.cE5E5E5
+                                      : ColorConst.c8875FF,
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    if (!isUser && showUserName)
+                                      Padding(
+                                        padding: EdgeInsets.only(bottom: 5.h),
+                                        child: Text(
+                                          userComment.userModel.fullName,
+                                          maxLines: 100,
+                                          style: PoppinsFont.w600.copyWith(
+                                            fontSize: 15.sp,
+                                            color: isUser
+                                                ? ColorConst.white
+                                                : ColorConst.blackWithOpacity087,
+                                          ),
                                         ),
                                       ),
+                                    Text(
+                                      userComment.commentModel.message,
+                                      maxLines: 100,
+                                      style: PoppinsFont.w400.copyWith(
+                                        fontSize: 15.sp,
+                                        color: isUser
+                                            ? ColorConst.white
+                                            : ColorConst.blackWithOpacity087,
+                                      ),
                                     ),
-                                  Text(
-                                    userComment.commentModel.message,
-                                    maxLines: 100,
-                                    style: PoppinsFont.w400.copyWith(
-                                      fontSize: 15.sp,
-                                      color: isUser
-                                          ? ColorConst.white
-                                          : ColorConst.blackWithOpacity087,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
                 isUser ? photoItem() : const SizedBox(),
               ],
